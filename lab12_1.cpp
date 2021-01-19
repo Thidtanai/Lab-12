ba30f1c0b1a28248e4f9020d7536888a16eb7083
Thidtanai <thidtanai_k@cmu.ac.th>
2021-01-19 16:55:15 +0700

    lab12_1complete

--- lab12_1.cpp
+++ lab12_1.cpp
@@ -17,3 +17,26 @@ int main(){
     cout << "\nMin = " << B[5];
     return 0;
 }
+
+void stat(const double inputArray[],int Number,double ans[]){
+    double AriMean, SD =0, GeoMean = 1;
+    double HarMean, Max , Min = inputArray[0];
+
+    for(int i = 0;i < Number;i++){
+      AriMean += inputArray[i];
+      GeoMean *= inputArray[i]; 
+      HarMean += 1/inputArray[i];
+      if(inputArray[i] > Max)Max = inputArray[i];
+      if(inputArray[i] < Min)Min = inputArray[i];
+    }
+    ans[0] = AriMean / Number;
+    for(int j=0;j<Number;j++){
+        SD += pow(inputArray[j]-ans[0],2);
+    }
+    ans[1] = pow((SD/Number),0.5);
+    ans[2] = pow(GeoMean,1/(double)Number);
+    ans[3] = Number/HarMean;
+    ans[4] = Max;
+    ans[5] = Min;
+    return;
+}
